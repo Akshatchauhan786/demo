f6ae5d562ee53984c210e8dc1fe0b384d8df0871
akshat chauhan <CHAUHANAKSHAT6@GMAIL.COM>
2023-11-10 15:53:33 +0530

    Login Signup routes

--- demo-project/src/app/signup/signup.component.ts
+++ demo-project/src/app/signup/signup.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  signupForm!: FormGroup; // Using definite assignment assertion
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.signupForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required],
+    });
+  }
+
+  onSignupSubmit() {
+    // Add your signup logic here
+    console.log(this.signupForm.value);
+  }
 }
